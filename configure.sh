3db39e311899001877e1dd232eea7240c5a6843f
Ross Gardler <ross.gardler@microsoft.com>
2016-03-10 01:00:51 -0800

    We didn't have Docker in there.

--- configure.sh
+++ configure.sh
@@ -28,6 +28,8 @@ sudo gem install tmuxinator
 
 # Docker
 
+sudo curl -sSL https://get.docker.com/ | sh
+
 curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
