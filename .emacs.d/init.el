85b10f6938ba52ef84749a7ddce4e5731a947dbe
Ross Gardler <ross@gardler.org>
2017-01-28 07:02:34 +0000

    add some useful packages and themes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,3 +16,71 @@
 		    ("\\.md\\'" . markdown-mode)
 		    ("\\.markdown\\'" . markdown-mode))
 	       :init (setq markdown-command "multimarkdown"))
+
+(mapc
+ (lambda (package)
+   (or (package-installed-p package)
+              (if (y-or-n-p (format "Package %s is missing.  Install it? " package))
+		  (package-install package))))
+ '
+
+ (bash-completion flycheck go-autocomplete go-direx direx
+  go-eldoc go-mode go-snippets highlight-indentation
+  python-environment smartparens zenburn-theme
+  auto-complete yasnippet zoom-window minimap
+  dockerfile-mode)
+)
+
+(load-theme 'zenburn-theme t)
+
+(require 'zoom-window)
+(global-set-key (kbd "C-x C-z") 'zoom-window-zoom)
+
+(require 'dockerfile-mode)
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(require 'bash-completion)
+(bash-completion-setup)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(require 'minimap)
+(setq minimap-update-delay 0.9)
+(setq minimap-window-location 'right)
+(setq minimap-recenter-type "free")
+
+(require 'auto-complete)
+(require 'auto-complete-config)
+(global-auto-complete-mode t)
+(setq tab-always-indent 'complete)
+(add-to-list 'completion-styles 'initials t)
+
+(require 'highlight-indentation)
+(add-hook 'go-mode-hook
+	  (lambda ()
+	    (highlight-indentation-current-column-mode)
+	    )
+	  )
+(set-face-background 'highlight-indentation-current-column-face "DarkSlateBlue")
+
+(declare-function sp-insert-pair "smartparens.el" nil)
+(require 'smartparens-config)
+(smartparens-global-mode 1)
+(show-smartparens-global-mode t)
+(sp-with-modes '(rhtml-mode)
+	       (sp-local-pair "<" ">")
+	       (sp-local-pair "<%" "%>"))
+(custom-set-faces
+ '(sp-show-pair-match-face ((t (:background "grey75" :foreground "darkslateblue" :inverse-video
+					     t))) nil "Changing weird sp match face highlight color for
+ console mode"))
+
+(require 'go-autocomplete)
+
+(require 'go-direx)
+(global-set-key (kbd "C-x C-j") 'go-direx-pop-to-buffer)
+
+(provide 'init)
