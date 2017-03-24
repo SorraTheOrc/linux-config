0004b126572104126b97adcbbc5d2903956e41b9
Ross Gardler <ross@gardler.org>
2017-03-24 06:40:27 +0000

    install java as sbt brings the latest development version which has brkoen certs

--- configure.sh
+++ configure.sh
@@ -37,6 +37,9 @@ sudo apt-get install -y lynx
 sudo apt-get install -y vi
 sudo apt-get install -y subversion
 
+## Java
+sudo apt-get install default-jdk
+
 ## Scala
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
