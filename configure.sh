338176d8ca2bcbe0fd11f07b3213dd9435cc8f0f
Ross Gardler <ross@gardler.org>
2017-10-13 16:16:57 -0700

    install docker-compose

--- configure.sh
+++ configure.sh
@@ -103,6 +103,11 @@ sudo systemctl enable docker
 sudo group add docker 
 sudo usermod -aG docker $USER
 
+## Docker-Compose
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # Kubernetes
 
 az acs kubernetes install-cli
