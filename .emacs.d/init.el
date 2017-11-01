5390c48d6ee4c5d1bbbe3f9d44ed03c7b6f7fef2
Ross Gardler <ross@gardler.org>
2017-11-01 16:45:25 -0700

    some global config improvements and fix up the package installation mechanism to use one consistent (and hopefully reliable) approach

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,39 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (require 'bind-key)
+
+;; global variables
+(setq
+ inhibit-startup-screen t
+ create-lockfiles nil
+ make-backup-files nil
+ column-number-mode t
+ scroll-error-top-bottom t
+ show-paren-delay 0.5
+ use-package-always-ensure t
+ sentence-end-double-space nil)
+
+;; buffer local variables
+(setq-default
+ indent-tabs-mode nil
+ tab-width 4
+ c-basic-offset 4)
+
+;; the package manager
+(require 'package)
+(setq
+ package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+		    ("org" . "http://orgmode.org/elpa/")
+		    ("melpa" . "http://melpa.org/packages/")
+		    ("melpa-stable" . "http://stable.melpa.org/packages/"))
+ package-archive-priorities '(("melpa-stable" . 1)))
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
 (require 'use-package)
 
-(require 'package)
-(add-to-list
- 'package-archives
- '("melpa" . "http://melpa.org/packages/")
- t)
-(package-initialize)
+;; Install / Configure Packages
 
 (use-package markdown-mode
 	     :ensure t
@@ -17,49 +43,51 @@
 		    ("\\.markdown\\'" . markdown-mode))
 	       :init (setq markdown-command "multimarkdown"))
 
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
 (use-package ensime
+  :ensure t
+  :pin melpa-stable)
+
+(use-package python-environment
   :ensure t)
 
-(mapc
- (lambda (package)
-   (or (package-installed-p package)
-              (if (y-or-n-p (format "Package %s is missing.  Install it? " package))
-		  (package-install package))))
- '
-
- (bash-completion flycheck go-autocomplete go-direx direx
-  go-eldoc go-mode go-snippets highlight-indentation
-  python-environment smartparens 
-  auto-complete yasnippet zoom-window minimap
-  dockerfile-mode)
-)
-
-(require 'zoom-window)
+(use-package zoom-window
+  :ensure t)
 (global-set-key (kbd "C-x C-z") 'zoom-window-zoom)
 
-(require 'dockerfile-mode)
+(use-package dockerfile-mode
+  :ensure t)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
 
-(require 'yasnippet)
-(yas-global-mode 1)
+(use-package yasnippet
+  :ensure t)
 
-(require 'bash-completion)
+(use-package bash-completion
+  :ensure t)
 (bash-completion-setup)
 
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-(require 'minimap)
+(use-package minimap
+  :ensure t)
 (setq minimap-update-delay 0.9)
 (setq minimap-window-location 'right)
 (setq minimap-recenter-type "free")
 
-(require 'auto-complete)
-(require 'auto-complete-config)
+(use-package auto-complete
+  :ensure t)
+
+(use-package auto-complete-config
+  :ensure t)
 (global-auto-complete-mode t)
 (setq tab-always-indent 'complete)
 (add-to-list 'completion-styles 'initials t)
 
-(require 'highlight-indentation)
+(use-package highlight-indentation
+  :ensure t)
 (add-hook 'go-mode-hook
 	  (lambda ()
 	    (highlight-indentation-current-column-mode)
@@ -67,22 +95,16 @@
 	  )
 (set-face-background 'highlight-indentation-current-column-face "DarkSlateBlue")
 
-(declare-function sp-insert-pair "smartparens.el" nil)
-(require 'smartparens-config)
-(smartparens-global-mode 1)
-(show-smartparens-global-mode t)
-(sp-with-modes '(rhtml-mode)
-	       (sp-local-pair "<" ">")
-	       (sp-local-pair "<%" "%>"))
-(custom-set-faces
- '(sp-show-pair-match-face ((t (:background "grey75" :foreground "darkslateblue" :inverse-video
-					     t))) nil "Changing weird sp match face highlight color for
- console mode"))
 
-(require 'go-autocomplete)
-
-(require 'go-direx)
-(global-set-key (kbd "C-x C-j") 'go-direx-pop-to-buffer)
+(use-package smartparens
+  :ensure t
+  :config
+  (setq sp-show-pair-from-inside nil)
+  (require 'smartparens-config)
+    :diminish smartparens-mode)
+(sp-pair "(" nil :unless '(sp-point-before-word-p))
+(sp-pair "[" nil :unless '(sp-point-before-word-p))
+(sp-pair "{" nil :unless '(sp-point-before-word-p))
 
 (progn
   ;; make indentation commands use space only (never tab character)
